ccdd834740da8e4082f32ba1fe755cc7a88d33f6
rike568 <140146232+rike568@users.noreply.github.com>
2025-11-07 10:19:54 +1000

    Added a 1x1 convulution layer which will be used to map feature channels to the number of classes

--- recognition/hipmri2d_unet_45807321/modules.py
+++ recognition/hipmri2d_unet_45807321/modules.py
@@ -114,3 +114,16 @@ class _LocalizationModule(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.block(x)
+
+class _SegmentationLayer(nn.Module):
+    """
+    Corresponds to 'segmentation layer' in the diagram.
+    A single 1x1 Conv2d.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.conv(x)
